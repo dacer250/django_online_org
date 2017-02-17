3a21c6922d8b3988f480e888fc1af3770c19d076
koeelio.MBP <koeelio@163.com>
2017-02-18 01:04:17 +0800

    commit

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,4 +1,15 @@
+from django.contrib.auth import authenticate,login
 from django.shortcuts import render
+
 
 # Create your views here.
 def login(request):
+    if request.method == "POST":
+        user_name = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        user = authenticate(user_name, password)
+        if user is not None:
+            login(request,user)
+            return render(request,"index.html")
+    elif request.method == 'GET':
+        return render(request, 'login.html', {})
