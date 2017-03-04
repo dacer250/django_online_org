37d20131193982f1200cb7ead8030dd72348542f
koeelio.MBP <koeelio@163.com>
2017-03-04 14:31:41 +0800

    commit

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -7,7 +7,7 @@ from django.views.generic import View
 from pure_pagination import Paginator, PageNotAnInteger
 
 from courses.models import Course, CourseResource
-from operation.models import UserFavorite, CourseComments
+from operation.models import UserFavorite, CourseComments, UserCourse
 
 
 class CourseListView(View):
@@ -57,7 +57,8 @@ class CourseDetailView(View):
             relate_courses = Course.objects.filter(tag=tag)[:1]
         else:
             relate_courses = []
-        return render(request, 'course-detail.html', {'course': course,
+        return render(request, 'course-detail.html',
+                      {'course': course,
                                                       'relate_courses': relate_courses,
                                                       'has_fav_org': has_fav_org,
                                                       'has_fav_course': has_fav_course,
@@ -67,9 +68,16 @@ class CourseDetailView(View):
 class CourseInfoView(View):
     def get(self, request, course_id):
         course = Course.objects.get(id=int(course_id))
+        user_coursers = UserCourse.objects.filter(course=course)
+        user_id = [user_courser.user.id for user_courser in user_coursers]
+        all_user_courses = UserCourse.objects.filter(user_id__in=user_id)
+        course_ids = [user_courser.course.id for user_courser in user_coursers]
+        relate_courses = Course.objects.filter(id__in=course_ids).order_by(
+            '-click_nums')[:5]
         all_resources = CourseResource.objects.filter(course=course)
         return render(request, "course-video.html", {"course": course,
                                                      "all_resources": all_resources,
+                                                     'relate_courses': relate_courses,
                                                      })
 
 
---
+++ apps/utils/mixin_utils.py
@@ -0,0 +1,3 @@
+# coding=utf-8
+__author__ = 'lihao'
+__date__ = '2017/3/4 14:31'
